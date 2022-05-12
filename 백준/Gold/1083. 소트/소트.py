31c5c7c85d24bf607ebdfa95c9292470130a9b97
lass9436 <57583282+lass9436@users.noreply.github.com>
2022-05-13 00:22:24 +0900

    [Gold IV] Title: 소트, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -6,22 +6,18 @@ arr = list(map(int, sys.stdin.readline().split()))
 
 s = int(sys.stdin.readline())
 
-while True:
-    change = False
-    for i in range(n):
-        targetIdx = i
-        for j in range(i+1, n):
-            if (j - i) > s:
-                break
-            if arr[j] > arr[targetIdx]:
-                targetIdx = j
-        if targetIdx != i:
-            change = True
-            temp = arr[targetIdx]
-            del arr[targetIdx]
-            arr.insert(i, temp)
-            s -= (targetIdx - i)
-    if not change:
-        break
+for i in range(n):
+    targetIdx = i
+    for j in range(i+1, n):
+        if (j - i) > s:
+            break
+        if arr[j] > arr[targetIdx]:
+            targetIdx = j
+    if targetIdx != i:
+
+        temp = arr[targetIdx]
+        del arr[targetIdx]
+        arr.insert(i, temp)
+        s -= (targetIdx - i)
         
 print(" ".join(map(str, arr)))
