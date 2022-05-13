04fe3641b2f8ca3d1dda61e08f5fcc97751d424e
lass9436 <57583282+lass9436@users.noreply.github.com>
2022-05-14 06:29:40 +0900

    [Silver III] Title: N과 M (1), Time: 204 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -5,13 +5,11 @@ arr = []
 def search(depth):
     if depth == M:
         print(" ".join(map(str, arr)))
-        arr.pop()
         return
     for i in range(1, N+1):
         if not i in arr:
             arr.append(i)
             search(depth+1)
-    if arr:
-        arr.pop()
+            arr.pop()
         
 search(0)
