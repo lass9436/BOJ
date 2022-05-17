5d81873f36f80ca931649afa8058e22c9dbc60df
lass9436 <57583282+lass9436@users.noreply.github.com>
2022-05-18 02:39:28 +0900

    [Gold IV] Title: 문자열 폭발, Time: 1224 ms, Memory: 39384 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+line = str(sys.stdin.readline().strip())
+bomb = str(sys.stdin.readline().strip())
+
+l = deque()
+
+for i in line:
+    l.append(i)
+    if len(l) >= len(bomb):
+        tof = True
+        for idx in range(1, len(bomb)+1):
+            if l[-idx] != bomb[-idx]:
+                tof = False
+                break
+        if tof:
+            for j in range(len(bomb)):
+                l.pop()
+
+if len(l) == 0:
+    print("FRULA")
+else:
+    for i in l:
+        print(i, end="")
