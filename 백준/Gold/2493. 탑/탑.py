d375e351a0780442e46dca89b3eec488babdbda7
lass9436 <57583282+lass9436@users.noreply.github.com>
2022-06-03 21:07:28 +0900

    [Gold V] Title: 탑, Time: 880 ms, Memory: 124780 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,27 +1,19 @@
 import sys
-r = sys.stdin.readline
+from collections import deque
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+answer = [0] * (N)
+queue = deque()
 
-n = int(r())
-t_list = list(map(int, r().split()))
-answer = []
-stk = []
-for i in range(n):
-    h = t_list[i]
-    if stk:
-        #print(stk)
-        while stk:
-            if stk[-1][0] < h :
-                stk.pop()
-                if not stk:
-                    print(0, end=' ')
-            elif stk[-1][0] > h:
-                print(stk[-1][1]+1, end=' ')
-                break
-            else : 
-                print(stk[-1][1]+1, end=' ')
-                stk.pop()
-                break
-        stk.append([h, i])
-    else:
-        print(0, end=' ')
-        stk.append([h,i])
+for i in range(N):
+    while queue and queue[-1][1] < arr[i]:
+        index, height = queue.pop()
+        if queue:
+            targetIndex, targetHeight = queue[-1]
+            answer[index] = targetIndex + 1
+    if queue:
+        index, height = queue[-1]
+        answer[i] = index + 1        
+    queue.append((i, arr[i]))
+    
+print(*answer)
