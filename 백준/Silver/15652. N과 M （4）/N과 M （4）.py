5959c200332387142be1e50281b8379395c2cc18
lass9436 <57583282+lass9436@users.noreply.github.com>
2022-06-12 06:05:16 +0900

    [Silver III] Title: N과 M (4), Time: 96 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**7)
+N, M = map(int, sys.stdin.readline().split())
+arr = []
+def dfs():
+    global arr
+    if len(arr) == M:
+        print(*arr)
+        return
+    for i in range(1, N+1):
+        if len(arr) == 0:
+            arr.append(i)
+            dfs()
+            arr.pop()
+        else:
+            pre = arr[-1]
+            if i >= pre:
+                arr.append(i)
+                dfs()
+                arr.pop()
+dfs()
